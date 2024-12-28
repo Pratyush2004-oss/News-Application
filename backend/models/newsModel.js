a357b6dcc8813cf6d3ca99ff48d9a2687304e0f2
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-12-28 23:34:41 +0530

    NEWS

---
+++ backend/models/newsModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const newsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+    source: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+});
+
+export const newsModel = mongoose.model("news", newsSchema);
