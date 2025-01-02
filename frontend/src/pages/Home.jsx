d7598277998c0945645fb6be7ea6aa8862256e9a
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-01-02 22:31:41 +0530

    DEmo

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../components/ui/button'
+
+const Home = () => {
+  return (
+    <div>
+    <H1>Home</H1>
+    <Button>Home</Button>
+    </div>
+  )
+}
+
+export default Home
